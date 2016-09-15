ecd889698d05c9970b74297f43c24ca79775ec52
vaezt <1519560753@qq.com>
2016-09-15 13:57:11 +0800

    fix some bug

--- FinalHomeWork/FinalHomeWork/main.cpp
+++ FinalHomeWork/FinalHomeWork/main.cpp
@@ -8,9 +8,26 @@ int main()
 	cout << "Hello Fucking man!";
 	List<int> list;
 	list.Add(5).Add(23).Add(34);
-	cout << list[1];
+	cout << endl;
+	list = list.Map<int>([](int i)->int { return ++i; });
+	list.ForEach([](int& i)-> void
+	{
+		cout << i << " ";
+	});
+	cout << endl << list[1];
 	list.RemoveAt(1);
-	list.Remove([](int& i) -> bool {return i == 5; });
-	list.ForEach([](int& i)->void {cout << i << " "; })
+	list.Remove([](int& i) -> bool
+	{
+		return i == 5;
+	});
+	try
+	{
+		List<int>* pList = &list;
+		delete pList;
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what();
+	}
 	return 0;
 }
