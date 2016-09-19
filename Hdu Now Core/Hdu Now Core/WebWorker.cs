a536b9eb527c3987ca79378dabfed9f7baa01864
Zeeko Zhu <vaezt@outlook.com>
2016-09-19 10:26:36 +0800

    添加了课表项目

---
+++ Hdu Now Core/Hdu Now Core/WebWorker.cs
@@ -0,0 +1,39 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hdu_Now_Core
+{
+    public class WebWorker
+    {
+        protected HtmlWeb HtmlWeb;
+        private string Url;
+        protected CookieContainer cookies;
+
+        public WebWorker(string url)
+        {
+            Url = url;
+            cookies = new CookieContainer();
+        }
+
+        public HtmlDocument Document;
+
+        public async Task Load()
+        {
+            Document = await HtmlWeb.LoadFromWebAsync(Url);
+        }
+
+        public async Task SendLoginRequest(string usr, string pwd)
+        {
+            HttpRequest request = WebRequest.Create(Url) as HttpWebRequest;
+            request.Method = "GET";
+            request.CookieContainer = cookies;
+        }
+
+    }
+}
