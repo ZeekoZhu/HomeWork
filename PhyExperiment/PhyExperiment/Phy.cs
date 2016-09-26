57ba9721f5cfb4afad912605df5dd1bc2dbd2303
Zeeko Zhu <vaezt@outlook.com>
2016-09-26 11:25:41 +0800

    添加了计算不确定度的小程序

---
+++ PhyExperiment/PhyExperiment/Phy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhyExperiment
+{
+    class Phy
+    {
+        public static double ComputeA(List<double> datas)
+        {
+            double avg = datas.Sum() / datas.Count;
+            double res = Math.Sqrt(
+                datas.Sum(
+                    (d) => Math.Pow((d - avg), 2)
+                    ) / (datas.Count * (datas.Count - 1)));
+            return res;
+        }
+
+        public static double ComputeB(double error) => erroe/Math.
+    }
+}
