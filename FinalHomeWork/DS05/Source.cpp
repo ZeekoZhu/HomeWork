1e4a43c3125c24f9ba649e1d7fb1c861a679d060
vaezt <1519560753@qq.com>
2016-11-06 15:06:44 +0800

    完成了数据结构第五次作业

--- FinalHomeWork/DS05/Source.cpp
+++ FinalHomeWork/DS05/Source.cpp
@@ -1,9 +1,144 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+#define MAXSIZE 10000
+#define MAXEDGE 100
+
+using namespace std;
+
+/// <summary>
+/// Converts string to int.
+/// </summary>
+/// <param name="s">The string.</param>
+/// <returns></returns>
+int Str2Int(string s)
+{
+    int res = 0;
+    for (char c : s)
+    {
+        res = res * 10 + c - '0';
+    }
+    return res;
+}
+
+class Matrix
+{
+public:
+    int Index = 0;
+    int MaxRow = 0;
+    int MaxCol = -1;
+    int V[MAXSIZE];
+    int B[MAXEDGE][MAXEDGE];
+    
+    /// <summary>
+    /// Gets the matrix from console.
+    /// </summary>
+    void GetMatrix()
+    {
+        cout << "请输入一个矩阵，元素间使用空格分隔，使用'\\'换行" << endl;
+        string tmp;
+        int col = 0;
+        while ((cin >> tmp) && (MaxCol < 0 || col - 1 <= MaxCol))
+        {
+            // if reads '\' then shifts to new row
+            if (tmp == "\\")
+            {
+                MaxCol = MaxCol < 0 ? col - 1 : MaxCol;
+                MaxRow++;
+                col = 0;
+            }
+            else
+            {
+                int num = Str2Int(tmp);
+                if (num)
+                {
+                    V[Index++] = num;
+                }
+                // if num != 0, add the value to the data domain
+                B[MaxRow][col] = num == 0 ? 0 : 1;
+                col++;
+            }
+        }
+        MaxCol = MaxCol < 0 ? col - 1 : MaxCol;
+        cin.clear();
+    }
+    
+    /// <summary>
+    /// Adds the specified matrix b.
+    /// </summary>
+    /// <param name="b">The b.</param>
+    /// <returns>The sum.</returns>
+    Matrix& Add(Matrix& b)
+    {
+        Matrix result;
+        result.MaxCol = b.MaxCol;
+        result.MaxRow = b.MaxRow;
+        int ia = 0;
+        int ib = 0;
+        for (int i = 0; i <= b.MaxRow; i++)
+        {
+            for (int j = 0; j <= b.MaxCol; j++)
+            {
+                result.B[i][j] = B[i][j] | b.B[i][j];
+                int tmp = 0;
+                if (B[i][j])
+                {
+                    tmp += V[ia++];
+                }
+                if (b.B[i][j])
+                {
+                    tmp += b.V[ib++];
+                }
+                // if the sum != 0, then append it to the data domain.
+                if (tmp)
+                {
+                    result.V[result.Index++] = tmp;
+                }
+            }
+        }
+        return result;
+    }
+    
+    /// <summary>
+    /// Prints this matrix.
+    /// </summary>
+    void Print()
+    {
+        int index = 0;
+        for (int i = 0; i <= MaxRow; i++)
+        {
+            for (int j = 0; j <= MaxCol; j++)
+            {
+                if (B[i][j])
+                {
+                    cout << V[index++] << " ";
+                }
+                else
+                {
+                    cout << "0 ";
+                }
+            }
+            cout << endl;
+        }
+    }
+
+};
+
+
+
+
+
 int main()
 {
-    
+    Matrix a;
+    a.GetMatrix();
+    Matrix b;
+    b.GetMatrix();
+    Matrix c = a.Add(b);
+    c.Print();
     return 0;
 }
+
+
--- FinalHomeWork/DS05/Tuple.h
+++ FinalHomeWork/DS05/Tuple.h
@@ -1,8 +1,62 @@
 #pragma once
+#include<vector>
 
 template<typename T>
 class Tuple
 {
+public:
     int Col;
     T Data;
+
+    Tuple() :Col(-1), Data(0)
+    {
+    }
+};
+
+template<typename T>
+class TupleMatrix
+{
+public:
+    int* Row;
+    Tuple<T>* Tuples;
+    int RowCount;   // 行数
+    int ColCount;   // 列数
+    int Tn;         // 非零元个数
+
+
+/// <summary>
+/// Gets the value.
+/// </summary>
+/// <param name="row">The row index.</param>
+/// <param name="col">The col index.</param>
+/// <returns></returns>
+    T& GetValue(int row, int col)
+    {
+        if (row<1 || row>RowCount || col<1 || col>ColCount)
+        {
+            throw new std::out_of_range("Index out of range");
+        }
+        // get the begining position of the row
+        int that = Row[row - 1];
+        // get the end position of the row
+        int end = row != RowCount ? Row[row] : Tn;
+        for (int i = 0; i < col && that != end; i++)
+        {
+            // col index is matched, we find the element
+            if (Tuples[that + i].Col == col)
+            {
+                return Tuples[that + i];
+            }
+            // col index is less than target, we should go futher
+            else if (Tuples[that+i].Col < col)
+            {
+                continue;
+            }
+            // col index is greater than target. return the defualt value of T
+            else
+            {
+                return *(new T());
+            }
+        }
+    }
 };
