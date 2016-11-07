54767e7cd0831d5325d3a61b0262749dd7fa0996
Zeeko Zhu <vaezt@outlook.com>
2016-11-07 11:34:15 +0800

    添加了输出提示

--- FinalHomeWork/FinalHomeWork/main.cpp
+++ FinalHomeWork/FinalHomeWork/main.cpp
@@ -29,18 +29,19 @@ int main()
         fout << 12 << " " << "hello" << " " << 12.244;
         fout.close();
     }*/
-    /*auto f1 = Student::ReadFile("1.txt");
+    auto f1 = Student::ReadFile("1.txt");
     auto f2 = Student::ReadFile("2.txt");
     List<Student> f3 = f1;
     f3.AddRange(f2);
     Student::WriteFile("3.txt", f3);
-    List<Student>* again = f3.Where([](Student s)->bool
+    cout << "数据已经合并到 3.txt" << endl;
+    List<Student>* again = f3.Where([](Student& s)->bool
     {
         return s.Chinese < 60 || s.English < 60 || s.Math < 60;
     });
-    Student::WriteFile("4.txt",*again);*/
-    
-    CollectionTest::ListSortTest();
+    Student::WriteFile("4.txt", *again);
+    cout << "补考学生名单已经写入 4.txt" << endl;
+
 
     return 0;
 }
