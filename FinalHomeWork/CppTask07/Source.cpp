896ee04bdef44c8fbd1953687f500dda70e7ebe5
Zeeko Zhu <vaezt@outlook.com>
2016-11-13 00:40:15 +0800

    cpp 达成

--- FinalHomeWork/CppTask07/Source.cpp
+++ FinalHomeWork/CppTask07/Source.cpp
@@ -5,23 +5,98 @@ template<typename T>
 class Vector
 {
     T* data;
-    int length;
 public:
-    Vector<T>() :length(0),data(NULL)
+    int Length;
+    int Capacity;
+    Vector<T>() : Length(0), Capacity(10)
     {
+        data = new T[10];
     }
 
-    Vector(const Vector<T>& other)
+    Vector<T>(int size) : Length(0), Capacity(size)
     {
-        if (data!=NULL)
+        data = new T[size];
+    }
+
+    Vector<T>(const Vector<T>& other)
+    {
+        if (data != NULL)
         {
             delete[] data;
         }
-        
+        data = new T[other.Length * 2];
+        Length = other.Length;
+        Capacity = other.Length * 2;
+        memcpy(data, other.data, sizeof(T)*Length);
+    }
+
+    Vector<T>& operator=(const Vector<T>& other)
+    {
+        if (data != NULL)
+        {
+            delete[] data;
+        }
+        data = new T[other.Length * 2];
+        Length = other.Length;
+        Capacity = other.Length * 2;
+        memcpy(data, other.data, sizeof(T)*Length);
+        return *this;
+    }
+
+    void Push(const T& value)
+    {
+        if (Length >= Capacity)
+        {
+            T* newData = new T[Capacity * 2];
+            Capacity *= 2;
+            memcpy(newData, data, sizeof(T)*Capacity);
+            delete[] data;
+            data = newData;
+        }
+        data[Length++] = value;
+    }
+
+    T operator[](int index)
+    {
+        return data[index];
     }
 };
 
 int main()
 {
-
+    int m, n;
+    cin >> m >> n;
+    Vector<int> vs[3];
+    Vector<int>* pv = new Vector<int>(m);
+    for (int i = 0; i < m; i++)
+    {
+        pv->Push(i + 1);
+    }
+    vs[0] = *pv;
+    delete pv;
+    pv = new Vector<int>(n);
+    for (int i = 0; i < n; i++)
+    {
+        pv->Push(i * 2);
+    }
+    vs[1] = *pv;
+    delete pv;
+    vs[2] = vs[1];
+    while (cin>>m)
+    {
+        cin >> n;
+        vs[m - 1].Push(n);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < vs[i].Length; j++)
+        {
+            cout << vs[i][j];
+            if (j!=vs[i].Length-1)
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 }
