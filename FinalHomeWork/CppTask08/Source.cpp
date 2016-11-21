127a44590debb1270d05554d22365ae0d5e7d32a
Zeeko Zhu <vaezt@outlook.com>
2016-11-21 12:31:11 +0800

    先跑到 Clion 上写代码。。。

--- FinalHomeWork/CppTask08/Source.cpp
+++ FinalHomeWork/CppTask08/Source.cpp
@@ -9,33 +9,34 @@ struct Block
     int X;
     int Y;
 
-    Block() :X(0), Y(0)
-    { }
-    Block(int x, int y) :X(x), Y(y)
-    { }
+    Block() : X(0), Y(0)
+    {}
+
+    Block(int x, int y) : X(x), Y(y)
+    {}
 };
 
 class Matrix
 {
-    vector<vector<int> >* datas;
-    vector<vector<bool> >* map;
+    vector<vector<int> > *datas;
+    vector<vector<bool> > *map;
     int row;
     int column;
     int visited;
 public:
     Matrix(int row, int column) :
-        datas(new vector<vector<int>>(row, vector<int>(column, 0))),
-        map(new vector<vector<bool>>(row, vector<bool>(column, false))),
-        row(row), column(column), visited(0)
+            datas(new vector<vector<int>>(row, vector<int>(column, 0))),
+            map(new vector<vector<bool>>(row, vector<bool>(column, false))),
+            row(row), column(column), visited(0)
     {
     }
 
-    bool IsVisited(Block* block) const
+    bool IsVisited(Block *block) const
     {
         return (*map)[block->X][block->Y];
     }
 
-    void Visit(Block* block)
+    void Visit(Block *block)
     {
         visited++;
         (*map)[block->X][block->Y] = true;
@@ -43,12 +44,12 @@ public:
 
     bool IsVisitedOut() const
     {
-        return visited >= row*column;
+        return visited >= row * column;
     }
 
-    Block* GetDownWithIt() const
+    Block *GetDownWithIt() const
     {
-        Block* block = nullptr;
+        Block *block = nullptr;
         for (int i = 0; i < row; ++i)
         {
             for (int j = 0; j < column; ++j)
@@ -63,7 +64,7 @@ public:
         return block;
     }
 
-    int GetValue(Block* block) const
+    int GetValue(Block *block) const
     {
         return (*datas)[block->X][block->Y];
     }
@@ -73,7 +74,7 @@ public:
         (*datas)[row][column] = value;
     }
 
-    Block* GetBeLowBlcok(Block* block) const
+    Block *GetBeLowBlock(Block *block) const
     {
         int x = block->X + 1;
         int y = block->Y;
@@ -85,7 +86,7 @@ public:
         return new Block(x, y);
     }
 
-    Block* GetRightBlock(Block* block) const
+    Block *GetRightBlock(Block *block) const
     {
         int x = block->X;
         int y = block->Y + 1;
@@ -115,11 +116,13 @@ int main()
             matrix.SetValue(i, j, tmp);
         }
     }
+    cout << "input end" << endl;
 
-    stack<Block*> trace;
-    Block* block = new Block();
+    stack<Block *> trace;
+    Block *block;
     while (!matrix.IsVisitedOut())
     {
+        block = matrix.GetDownWithIt();
         trace.push(block);
         matrix.Visit(block);
         int value = matrix.GetValue(block);
@@ -127,9 +130,8 @@ int main()
         {
             if (!matrix.IsVisited(block) && value == matrix.GetValue(block))
             {
-                trace.push(block);
                 matrix.Visit(block);
-                block = matrix.GetBeLowBlcok(block);
+                block = matrix.GetBeLowBlock(block);
             }
             else
             {
@@ -140,10 +142,9 @@ int main()
                 block = matrix.GetRightBlock(trace.top());
                 trace.pop();
             }
+            trace.push(block);
         }
         cout << value << endl;
     }
-
-
     return 0;
 }
